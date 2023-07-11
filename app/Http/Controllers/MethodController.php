319c0390bc08e96e6ff3c592b658df18f2045364
nicollasedgar <nicolas10edgar@gmail.com>
2023-07-11 22:28:07 +0700

    update controller method dan view method

--- app/Http/Controllers/MethodController.php
+++ app/Http/Controllers/MethodController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Weather;
+use Matrix\Matrix;
+
+// require '/../vendor/autoload.php';
 
 class MethodController extends Controller
 {
@@ -77,7 +80,16 @@ class MethodController extends Controller
         $humiditySunshineTotal = $results[0]->humidity_sunshine_total;
         $windspeedSunshineTotal = $results[0]->windspeed_sunshine_total;
 
-        $matrixA = [
+        // recently not use but in case looking for matrix H
+        $gridH = [
+            [$totalRaingauge],
+            [$temperatureRaingaugeTotal],
+            [$humidityRaingaugeTotal],
+            [$windspeedRaingaugeTotal],
+            [$sunshineRaingaugeTotal],
+        ];
+
+        $gridA = [
             [$totalRows, $totalTemperature, $totalHumidity, $totalWindspeed, $totalSunshine],
             [$totalTemperature, $totalSquaredTemperature, $temperatureHumidityTotal, $temperatureWindspeedTotal, $temperatureSunshineTotal],
             [$totalHumidity, $temperatureHumidityTotal, $totalSquaredHumidity, $humidityWindspeedTotal, $humiditySunshineTotal],
@@ -85,57 +97,105 @@ class MethodController extends Controller
             [$totalSunshine, $temperatureSunshineTotal, $humiditySunshineTotal, $windspeedSunshineTotal, $totalSquaredSunshine],
         ];
 
-        $matrixH = [
-            [$totalRows],
-            [$temperatureRaingaugeTotal],
-            [$humidityRaingaugeTotal],
-            [$windspeedRaingaugeTotal],
-            [$sunshineRaingaugeTotal],
-        ];
-
-        $matrixA1 = [
-            [$totalRows, $totalTemperature, $totalHumidity, $totalWindspeed, $totalSunshine],
+        $gridA1 = [
+            [$totalRaingauge, $totalTemperature, $totalHumidity, $totalWindspeed, $totalSunshine],
             [$temperatureRaingaugeTotal, $totalSquaredTemperature, $temperatureHumidityTotal, $temperatureWindspeedTotal, $temperatureSunshineTotal],
             [$humidityRaingaugeTotal, $temperatureHumidityTotal, $totalSquaredHumidity, $humidityWindspeedTotal, $humiditySunshineTotal],
             [$windspeedRaingaugeTotal, $temperatureWindspeedTotal, $humidityWindspeedTotal, $totalSquaredWindspeed, $windspeedSunshineTotal],
             [$sunshineRaingaugeTotal, $temperatureSunshineTotal, $humiditySunshineTotal, $windspeedSunshineTotal, $totalSquaredSunshine],
         ];
 
-        $matrixA2 = [
-            [$totalRows, $totalRows, $totalHumidity, $totalWindspeed, $totalSunshine],
+        $gridA2 = [
+            [$totalRows, $totalRaingauge, $totalHumidity, $totalWindspeed, $totalSunshine],
             [$totalTemperature, $temperatureRaingaugeTotal, $temperatureHumidityTotal, $temperatureWindspeedTotal, $temperatureSunshineTotal],
             [$totalHumidity, $humidityRaingaugeTotal, $totalSquaredHumidity, $humidityWindspeedTotal, $humiditySunshineTotal],
             [$totalWindspeed, $windspeedRaingaugeTotal, $humidityWindspeedTotal, $totalSquaredWindspeed, $windspeedSunshineTotal],
             [$totalSunshine, $sunshineRaingaugeTotal, $humiditySunshineTotal, $windspeedSunshineTotal, $totalSquaredSunshine],
         ];
 
-        $matrixA3 = [
-            [$totalRows, $totalTemperature, $totalRows, $totalWindspeed, $totalSunshine],
+        $gridA3 = [
+            [$totalRows, $totalTemperature, $totalRaingauge, $totalWindspeed, $totalSunshine],
             [$totalTemperature, $totalSquaredTemperature, $temperatureRaingaugeTotal, $temperatureWindspeedTotal, $temperatureSunshineTotal],
             [$totalHumidity, $temperatureHumidityTotal, $humidityRaingaugeTotal, $humidityWindspeedTotal, $humiditySunshineTotal],
             [$totalWindspeed, $temperatureWindspeedTotal, $windspeedRaingaugeTotal, $totalSquaredWindspeed, $windspeedSunshineTotal],
             [$totalSunshine, $temperatureSunshineTotal, $sunshineRaingaugeTotal, $windspeedSunshineTotal, $totalSquaredSunshine],
         ];
 
-        $matrixA4 = [
-            [$totalRows, $totalTemperature, $totalHumidity, $totalRows, $totalSunshine],
+        $gridA4 = [
+            [$totalRows, $totalTemperature, $totalHumidity, $totalRaingauge, $totalSunshine],
             [$totalTemperature, $totalSquaredTemperature, $temperatureHumidityTotal, $temperatureRaingaugeTotal, $temperatureSunshineTotal],
             [$totalHumidity, $temperatureHumidityTotal, $totalSquaredHumidity, $humidityRaingaugeTotal, $humiditySunshineTotal],
             [$totalWindspeed, $temperatureWindspeedTotal, $humidityWindspeedTotal, $windspeedRaingaugeTotal, $windspeedSunshineTotal],
             [$totalSunshine, $temperatureSunshineTotal, $humiditySunshineTotal, $sunshineRaingaugeTotal, $totalSquaredSunshine],
         ];
 
-        $matrixA5 = [
-            [$totalRows, $totalTemperature, $totalHumidity, $totalWindspeed, $totalRows],
+        $gridA5 = [
+            [$totalRows, $totalTemperature, $totalHumidity, $totalWindspeed, $totalRaingauge],
             [$totalTemperature, $totalSquaredTemperature, $temperatureHumidityTotal, $temperatureWindspeedTotal, $temperatureRaingaugeTotal],
             [$totalHumidity, $temperatureHumidityTotal, $totalSquaredHumidity, $humidityWindspeedTotal, $humidityRaingaugeTotal],
             [$totalWindspeed, $temperatureWindspeedTotal, $humidityWindspeedTotal, $totalSquaredWindspeed, $windspeedRaingaugeTotal],
             [$totalSunshine, $temperatureSunshineTotal, $humiditySunshineTotal, $windspeedSunshineTotal, $sunshineRaingaugeTotal],
         ];
 
+        $matrixA = new Matrix($gridA);
+        $matrixA1 = new Matrix($gridA1);
+        $matrixA2 = new Matrix($gridA2);
+        $matrixA3 = new Matrix($gridA3);
+        $matrixA4 = new Matrix($gridA4);
+        $matrixA5 = new Matrix($gridA5);
+
+        $detMatrixA = $matrixA->determinant();
+        $detMatrixA1 = $matrixA1->determinant();
+        $detMatrixA2 = $matrixA2->determinant();
+        $detMatrixA3 = $matrixA3->determinant();
+        $detMatrixA4 = $matrixA4->determinant();
+        $detMatrixA5 = $matrixA5->determinant();
+
+        $koefisienb0 = $detMatrixA / $detMatrixA1;
+        $koefisienb1 = $detMatrixA / $detMatrixA2;
+        $koefisienb2 = $detMatrixA / $detMatrixA3;
+        $koefisienb3 = $detMatrixA / $detMatrixA4;
+        $koefisienb4 = $detMatrixA / $detMatrixA5;
+
         return view('method', [
             "title" => "Method",
-            "total" => $totalSquaredHumidity,
+            "temperatureRaingaugeTotal" => $temperatureRaingaugeTotal,
+            "humidityRaingaugeTotal" => $humidityRaingaugeTotal,
+            "windspeedRaingaugeTotal" => $windspeedRaingaugeTotal,
+            "sunshineRaingaugeTotal" => $sunshineRaingaugeTotal,
+            "temperatureHumidityTotal" => $temperatureHumidityTotal,
+            "temperatureWindspeedTotal" => $temperatureWindspeedTotal,
+            "temperatureSunshineTotal" => $temperatureSunshineTotal,
+            "humidityWindspeedTotal" => $humidityWindspeedTotal,
+            "humiditySunshineTotal" => $humiditySunshineTotal,
+            "windspeedSunshineTotal" => $windspeedSunshineTotal,
+            "totalSquaredTemperature" => $totalSquaredTemperature,
+            "totalSquaredHumidity" => $totalSquaredHumidity,
+            "totalSquaredWindspeed" => $totalSquaredWindspeed,
+            "totalSquaredSunshine" => $totalSquaredSunshine,
+            "totalRows" => $totalRows,
+            "totalTemperature" => $totalTemperature,
+            "totalHumidity" => $totalHumidity,
+            "totalWindspeed" => $totalWindspeed,
+            "totalSunshine" => $totalSunshine,
+            "totalRaingauge" => $totalRaingauge,
+            "matrixA" => $gridA,
+            "matrixA1" => $gridA1,
+            "matrixA2" => $gridA2,
+            "matrixA3" => $gridA3,
+            "matrixA4" => $gridA4,
+            "matrixA5" => $gridA5,
+            "detMatrixA" => $detMatrixA,
+            "detMatrixA1" => $detMatrixA1,
+            "detMatrixA2" => $detMatrixA2,
+            "detMatrixA3" => $detMatrixA3,
+            "detMatrixA4" => $detMatrixA4,
+            "detMatrixA5" => $detMatrixA5,
+            "koefisienb0" => $koefisienb0,
+            "koefisienb1" => $koefisienb1,
+            "koefisienb2" => $koefisienb2,
+            "koefisienb3" => $koefisienb3,
+            "koefisienb4" => $koefisienb4,
         ]);
     }
 }
--- resources/views/method.blade.php
+++ resources/views/method.blade.php
@@ -5,7 +5,254 @@
 <main class="main-content position-relative border-radius-lg ">
     @include('layout.navbar')
     <div class="container-fluid py-4">
-        @dd($totalkali, $totalkalilagi)
+        <div class="row">
+            <div class="col-12">
+              <div class="card mb-4">
+                <div class="card-header pb-0">
+                    <h6>Helper Table</h6>
+                </div>
+                <div class="card-body px-0 pt-0 pb-2">
+                  <div class="table-responsive p-0">
+                    <table class="table align-items-center mb-0">
+                      <thead>
+                        <tr>
+                          <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">∑X<sub>1</sub>Y</th>
+                          <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">∑X<sub>2</sub>Y</th>
+                          <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">∑X<sub>3</sub>Y</th>
+                          <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">∑X<sub>4</sub>Y</th>
+                          <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">∑X<sub>1</sub>X<sub>2</sub></th>
+                          <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">∑X<sub>1</sub>X<sub>3</sub></th>
+                          <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">∑X<sub>1</sub>X<sub>4</sub></th>
+                          <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">∑X<sub>2</sub>X<sub>3</sub></th>
+                          <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">∑X<sub>2</sub>X<sub>4</sub></th>
+                          <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">∑X<sub>3</sub>X<sub>4</sub></th>
+                          <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">∑X<sub>1</sub><sup>2</sup></th>
+                          <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">∑X<sub>2</sub><sup>2</sup></th>
+                          <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">∑X<sub>3</sub><sup>2</sup></th>
+                          <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">∑X<sub>4</sub><sup>2</sup></th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                            <td class="align-middle text-center"><span class="text-secondary text-xs font-weight-bold">{{ $temperatureRaingaugeTotal }}</span></td>
+                            <td class="align-middle text-center"><span class="text-secondary text-xs font-weight-bold">{{ $humidityRaingaugeTotal }}</span></td>
+                            <td class="align-middle text-center"><span class="text-secondary text-xs font-weight-bold">{{ $windspeedRaingaugeTotal }}</span></td>
+                            <td class="align-middle text-center"><span class="text-secondary text-xs font-weight-bold">{{ $sunshineRaingaugeTotal }}</span></td>
+                            <td class="align-middle text-center"><span class="text-secondary text-xs font-weight-bold">{{ $temperatureHumidityTotal }}</span></td>
+                            <td class="align-middle text-center"><span class="text-secondary text-xs font-weight-bold">{{ $temperatureWindspeedTotal }}</span></td>
+                            <td class="align-middle text-center"><span class="text-secondary text-xs font-weight-bold">{{ $temperatureSunshineTotal }}</span></td>
+                            <td class="align-middle text-center"><span class="text-secondary text-xs font-weight-bold">{{ $humidityWindspeedTotal }}</span></td>
+                            <td class="align-middle text-center"><span class="text-secondary text-xs font-weight-bold">{{ $humiditySunshineTotal }}</span></td>
+                            <td class="align-middle text-center"><span class="text-secondary text-xs font-weight-bold">{{ $windspeedSunshineTotal }}</span></td>
+                            <td class="align-middle text-center"><span class="text-secondary text-xs font-weight-bold">{{ $totalSquaredTemperature }}</span></td>
+                            <td class="align-middle text-center"><span class="text-secondary text-xs font-weight-bold">{{ $totalSquaredHumidity }}</span></td>
+                            <td class="align-middle text-center"><span class="text-secondary text-xs font-weight-bold">{{ $totalSquaredWindspeed }}</span></td>
+                            <td class="align-middle text-center"><span class="text-secondary text-xs font-weight-bold">{{ $totalSquaredSunshine }}</span></td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-7 mt-4">
+              <div class="card">
+                <div class="card-header pb-0 px-3">
+                  <h6 class="mb-0">Matrix Table</h6>
+                </div>
+                <div class="card-body pt-4 p-3">
+                  <ul class="list-group">
+
+                    <li class="list-group-item border-0 d-flex p-4 mb-2 bg-gray-100 border-radius-lg">
+                      <div class="d-flex flex-column">
+                        <h6 class="mb-3 text-sm">Matrix A</h6>
+                        <table class="table align-items-center mb-0">
+                            <tbody>
+                              @foreach ($matrixA as $data)
+                                  <tr>
+                                    @foreach ($data as $item)
+                                    <td class="align-middle text-center"><span class="text-secondary text-xs font-weight-bold">{{ $item }}</span></td>
+                                    @endforeach
+                                  </tr>
+                              @endforeach
+                            </tbody>
+                          </table>
+                      </div>
+                      <div class="ms-auto text-end">
+                        <p class="btn btn-link text-dark text-gradient px-3 mb-0">Det.</p>
+                        <p class="btn btn-link text-dark px-3 mb-0"><?= round($detMatrixA, 2); ?></p>
+                      </div>
+                    </li>
+                    <li class="list-group-item border-0 d-flex p-4 mb-2 bg-gray-100 border-radius-lg">
+                      <div class="d-flex flex-column">
+                        <h6 class="mb-3 text-sm">Matrix A1</h6>
+                        <table class="table align-items-center mb-0">
+                            <tbody>
+                              @foreach ($matrixA1 as $data)
+                                  <tr>
+                                    @foreach ($data as $item)
+                                    <td class="align-middle text-center"><span class="text-secondary text-xs font-weight-bold">{{ $item }}</span></td>
+                                    @endforeach
+                                  </tr>
+                              @endforeach
+                            </tbody>
+                          </table>
+                      </div>
+                      <div class="ms-auto text-end">
+                        <p class="btn btn-link text-dark text-gradient px-3 mb-0">Det.</p>
+                        <p class="btn btn-link text-dark px-3 mb-0"><?= round($detMatrixA1, 2); ?></p>
+                      </div>
+                    </li>
+                    <li class="list-group-item border-0 d-flex p-4 mb-2 bg-gray-100 border-radius-lg">
+                      <div class="d-flex flex-column">
+                        <h6 class="mb-3 text-sm">Matrix A2</h6>
+                        <table class="table align-items-center mb-0">
+                            <tbody>
+                              @foreach ($matrixA2 as $data)
+                                  <tr>
+                                    @foreach ($data as $item)
+                                    <td class="align-middle text-center"><span class="text-secondary text-xs font-weight-bold">{{ $item }}</span></td>
+                                    @endforeach
+                                  </tr>
+                              @endforeach
+                            </tbody>
+                          </table>
+                      </div>
+                      <div class="ms-auto text-end">
+                        <p class="btn btn-link text-dark text-gradient px-3 mb-0">Det.</p>
+                        <p class="btn btn-link text-dark px-3 mb-0"><?= round($detMatrixA2, 2); ?></p>
+                      </div>
+                    </li>
+                    <li class="list-group-item border-0 d-flex p-4 mb-2 bg-gray-100 border-radius-lg">
+                      <div class="d-flex flex-column">
+                        <h6 class="mb-3 text-sm">Matrix A3</h6>
+                        <table class="table align-items-center mb-0">
+                            <tbody>
+                              @foreach ($matrixA3 as $data)
+                                  <tr>
+                                    @foreach ($data as $item)
+                                    <td class="align-middle text-center"><span class="text-secondary text-xs font-weight-bold">{{ $item }}</span></td>
+                                    @endforeach
+                                  </tr>
+                              @endforeach
+                            </tbody>
+                          </table>
+                      </div>
+                      <div class="ms-auto text-end">
+                        <p class="btn btn-link text-dark text-gradient px-3 mb-0">Det.</p>
+                        <p class="btn btn-link text-dark px-3 mb-0"><?= round($detMatrixA3, 2); ?></p>
+                      </div>
+                    </li>
+                    <li class="list-group-item border-0 d-flex p-4 mb-2 bg-gray-100 border-radius-lg">
+                      <div class="d-flex flex-column">
+                        <h6 class="mb-3 text-sm">Matrix A4</h6>
+                        <table class="table align-items-center mb-0">
+                            <tbody>
+                              @foreach ($matrixA4 as $data)
+                                  <tr>
+                                    @foreach ($data as $item)
+                                    <td class="align-middle text-center"><span class="text-secondary text-xs font-weight-bold">{{ $item }}</span></td>
+                                    @endforeach
+                                  </tr>
+                              @endforeach
+                            </tbody>
+                          </table>
+                      </div>
+                      <div class="ms-auto text-end">
+                        <p class="btn btn-link text-dark text-gradient px-3 mb-0">Det.</p>
+                        <p class="btn btn-link text-dark px-3 mb-0"><?= round($detMatrixA4, 2); ?></p>
+                      </div>
+                    </li>
+                    <li class="list-group-item border-0 d-flex p-4 mb-2 bg-gray-100 border-radius-lg">
+                      <div class="d-flex flex-column">
+                        <h6 class="mb-3 text-sm">Matrix A5</h6>
+                        <table class="table align-items-center mb-0">
+                            <tbody>
+                              @foreach ($matrixA5 as $data)
+                                  <tr>
+                                    @foreach ($data as $item)
+                                    <td class="align-middle text-center"><span class="text-secondary text-xs font-weight-bold">{{ $item }}</span></td>
+                                    @endforeach
+                                  </tr>
+                              @endforeach
+                            </tbody>
+                          </table>
+                      </div>
+                      <div class="ms-auto text-end">
+                        <p class="btn btn-link text-dark text-gradient px-3 mb-0">Det.</p>
+                        <p class="btn btn-link text-dark px-3 mb-0"><?= round($detMatrixA5, 2); ?></p>
+                      </div>
+                    </li>
+
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-5 mt-4">
+              <div class="card h-40 mb-4">
+                <div class="card-body pt-4 p-3">
+                  <h6 class="text-uppercase text-body text-xs font-weight-bolder mb-3">Coefficien</h6>
+                  <ul class="list-group">
+
+                    <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+                      <div class="d-flex align-items-center">
+                        <div class="d-flex flex-column">
+                            <span class="text-xs">b0</span>
+                            <h6 class="mb-1 text-dark text-sm">{{ $koefisienb0 }}</h6>
+                        </div>
+                      </div>  
+                    </li>
+                    <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+                      <div class="d-flex align-items-center">
+                        <div class="d-flex flex-column">
+                            <span class="text-xs">b1</span>
+                            <h6 class="mb-1 text-dark text-sm">{{ $koefisienb1 }}</h6>
+                        </div>
+                      </div>  
+                    </li>
+                    <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+                      <div class="d-flex align-items-center">
+                        <div class="d-flex flex-column">
+                            <span class="text-xs">b2</span>
+                            <h6 class="mb-1 text-dark text-sm">{{ $koefisienb2 }}</h6>
+                        </div>
+                      </div>  
+                    </li>
+                    <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+                      <div class="d-flex align-items-center">
+                        <div class="d-flex flex-column">
+                            <span class="text-xs">b3</span>
+                            <h6 class="mb-1 text-dark text-sm">{{ $koefisienb3 }}</h6>
+                        </div>
+                      </div>  
+                    </li>
+                    <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+                      <div class="d-flex align-items-center">
+                        <div class="d-flex flex-column">
+                            <span class="text-xs">b4</span>
+                            <h6 class="mb-1 text-dark text-sm">{{ $koefisienb4 }}</h6>
+                        </div>
+                      </div>  
+                    </li>
+                    
+                  </ul>
+                  <h6 class="text-uppercase text-body text-xs font-weight-bolder my-3">Multiple Linear Regression Model</h6>
+                  <ul class="list-group">
+                    <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+                      <div class="d-flex align-items-center">
+                        <div class="d-flex flex-column">
+                          <h6 class="mb-1 text-dark text-sm">Y = {{ number_format($koefisienb0,4) }} + {{ number_format($koefisienb1,4) }}X<sub>1</sub> + {{ number_format($koefisienb2,4) }}X<sub>2</sub> + {{ number_format($koefisienb3,4) }}X<sub>3</sub> + {{ number_format($koefisienb4,4) }}X<sub>4</sub></h6>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
 
         @include('layout.footer')
     </div>
